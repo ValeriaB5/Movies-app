7d5b908ac6c905c5cd7e98be9b30cb2f2e4a9331
Valeria <vale.bedoya@outlook.com>
2023-07-22 17:00:40 -0500

    Ultimos cambios realizados

--- src/main.js
+++ src/main.js
@@ -1,1 +1,311 @@
-console.log('Hagamos un super proyecto')
+//NODES-------------------------------------------------------------------------------------------------------
+const API_KEY = '5f6fc6301fbe833c82b8fd21b7e25f19';
+//Aca seleccionamos las partes del HTML segun su CSS
+// Sections
+const headerSection = document.querySelector('#header');
+const trendingPreviewSection = document.querySelector('#trendingPreview');
+const categoriesPreviewSection = document.querySelector('#categoriesPreview');
+const genericSection = document.querySelector('#genericList');
+const movieDetailSection = document.querySelector('#movieDetail');
+
+// Lists & Containers
+const searchForm = document.querySelector('#searchForm');
+const trendingMoviesPreviewList = document.querySelector('.trendingPreview-movieList');
+const categoriesPreviewList = document.querySelector('.categoriesPreview-list');
+const movieDetailCategoriesList = document.querySelector('#movieDetail .categories-list');
+const relatedMoviesContainer = document.querySelector('.relatedMovies-scrollContainer');
+
+// Elements
+const headerTitle = document.querySelector('.header-title');
+const arrowBtn = document.querySelector('.header-arrow');
+const headerCategoryTitle = document.querySelector('.header-title--categoryView');
+
+const searchFormInput = document.querySelector('#searchForm input');
+const searchFormBtn = document.querySelector('#searchBtn');
+
+const trendingBtn = document.querySelector('.trendingPreview-btn');
+
+const movieDetailTitle = document.querySelector('.movieDetail-title');
+const movieDetailDescription = document.querySelector('.movieDetail-description');
+const movieDetailScore = document.querySelector('.movieDetail-score');
+
+//MAIN-------------------------------------------------------------------------------------------------------
+
+const api = axios.create({
+  baseURL: 'https://api.themoviedb.org/3/',
+  headers: {
+    'Content-Type': 'application/json;charset=utf-8',
+  },
+  params: {
+    'api_key': API_KEY,
+  },
+});
+
+
+// Utils
+
+function createMovies(movies, container) {
+  container.innerHTML = '';
+
+  movies.forEach(movie => {
+    const movieContainer = document.createElement('div');
+    movieContainer.classList.add('movie-container');
+    movieContainer.addEventListener('click', () => {
+      location.hash = '#movie=' + movie.id;
+    });
+
+    const movieImg = document.createElement('img');
+    movieImg.classList.add('movie-img');
+    movieImg.setAttribute('alt', movie.title);
+    movieImg.setAttribute(
+      'src',
+      'https://image.tmdb.org/t/p/w300' + movie.poster_path,
+    );
+
+    movieContainer.appendChild(movieImg);
+    container.appendChild(movieContainer);
+  });
+}
+
+function createCategories(categories, container) {
+  container.innerHTML = "";
+
+  categories.forEach(category => {  
+    const categoryContainer = document.createElement('div');
+    categoryContainer.classList.add('category-container');
+
+    const categoryTitle = document.createElement('h3');
+    categoryTitle.classList.add('category-title');
+    categoryTitle.setAttribute('id', 'id' + category.id);
+    categoryTitle.addEventListener('click', () => {
+      location.hash = `#category=${category.id}-${category.name}`;
+    });
+    const categoryTitleText = document.createTextNode(category.name);
+
+    categoryTitle.appendChild(categoryTitleText);
+    categoryContainer.appendChild(categoryTitle);
+    container.appendChild(categoryContainer);
+  });
+}
+
+// Llamados a la API
+
+async function getTrendingMoviesPreview() {
+  const { data } = await api('trending/movie/day');
+  const movies = data.results;
+  console.log(movies)
+
+  createMovies(movies, trendingMoviesPreviewList);
+}
+
+async function getCategegoriesPreview() {
+  const { data } = await api('genre/movie/list');
+  const categories = data.genres;
+
+  createCategories(categories, categoriesPreviewList)  ;
+}
+
+async function getMoviesByCategory(id) {
+  const { data } = await api('discover/movie', {
+    params: {
+      with_genres: id,
+    },
+  });
+  const movies = data.results;
+
+  createMovies(movies, genericSection);
+}
+
+async function getMoviesBySearch(query) {
+  const { data } = await api('search/movie', {
+    params: {
+      query,
+    },
+  });
+  const movies = data.results;
+
+  createMovies(movies, genericSection);
+}
+
+async function getTrendingMovies() {
+  const { data } = await api('trending/movie/day');
+  const movies = data.results;
+
+  createMovies(movies, genericSection);
+}
+
+async function getMovieById(id) {
+  const { data: movie } = await api('movie/' + id);
+
+  const movieImgUrl = 'https://image.tmdb.org/t/p/w500' + movie.poster_path;
+  console.log(movieImgUrl)
+  headerSection.style.background = `
+    linear-gradient(
+      180deg,
+      rgba(0, 0, 0, 0.35) 19.27%,
+      rgba(0, 0, 0, 0) 29.17%
+    ),
+    url(${movieImgUrl})
+  `;
+  
+  movieDetailTitle.textContent = movie.title;
+  movieDetailDescription.textContent = movie.overview;
+  movieDetailScore.textContent = movie.vote_average;
+
+  createCategories(movie.genres, movieDetailCategoriesList);
+
+  getRelatedMoviesId(id);
+}
+
+async function getRelatedMoviesId(id) {
+  const { data } = await api(`movie/${id}/recommendations`);
+  const relatedMovies = data.results;
+
+  createMovies(relatedMovies, relatedMoviesContainer);
+}
+  
+
+//NAVIGATION-----------------------------------------------------------------------------------------------------
+//Escuchamos los eventos de cada una de las secciones o paginas
+searchFormBtn.addEventListener('click', () => {
+  location.hash = '#search=' + searchFormInput.value;
+});
+
+trendingBtn.addEventListener('click', () => {
+  location.hash = '#trends';
+});
+
+arrowBtn.addEventListener('click', () => {
+  history.back();
+  // location.hash = '#home';
+});
+
+window.addEventListener('DOMContentLoaded', navigator, false);
+window.addEventListener('hashchange', navigator, false);
+
+function navigator() {
+  console.log({ location });
+  
+  if (location.hash.startsWith('#trends')) {
+    trendsPage();
+  } else if (location.hash.startsWith('#search=')) {
+    searchPage();
+  } else if (location.hash.startsWith('#movie=')) {
+    movieDetailsPage();
+  } else if (location.hash.startsWith('#category=')) {
+    categoriesPage();
+  } else {
+    homePage();
+  }
+
+  document.body.scrollTop = 0;
+  document.documentElement.scrollTop = 0;
+}
+
+function homePage() {
+  console.log('Home!!');
+
+  headerSection.classList.remove('header-container--long');
+  headerSection.style.background = '';
+  arrowBtn.classList.add('inactive');
+  arrowBtn.classList.remove('header-arrow--white');
+  headerTitle.classList.remove('inactive');
+  headerCategoryTitle.classList.add('inactive');
+  searchForm.classList.remove('inactive');
+
+  trendingPreviewSection.classList.remove('inactive');
+  categoriesPreviewSection.classList.remove('inactive');
+  genericSection.classList.add('inactive');
+  movieDetailSection.classList.add('inactive');
+  
+  getTrendingMoviesPreview();
+  getCategegoriesPreview();
+}
+
+function categoriesPage() {
+  console.log('categories!!');
+
+  headerSection.classList.remove('header-container--long');
+  headerSection.style.background = '';
+  arrowBtn.classList.remove('inactive');
+  arrowBtn.classList.remove('header-arrow--white');
+  headerTitle.classList.add('inactive');
+  headerCategoryTitle.classList.remove('inactive');
+  searchForm.classList.add('inactive');
+
+  trendingPreviewSection.classList.add('inactive');
+  categoriesPreviewSection.classList.add('inactive');
+  genericSection.classList.remove('inactive');
+  movieDetailSection.classList.add('inactive');
+
+  // ['#category', 'id-name']
+  const [_, categoryData] = location.hash.split('=');
+  const [categoryId, categoryName] = categoryData.split('-');
+
+  headerCategoryTitle.innerHTML = categoryName;
+  
+  getMoviesByCategory(categoryId);
+}
+
+function movieDetailsPage() {
+  console.log('Movie!!');
+
+  headerSection.classList.add('header-container--long');
+  // headerSection.style.background = '';
+  arrowBtn.classList.remove('inactive');
+  arrowBtn.classList.add('header-arrow--white');
+  headerTitle.classList.add('inactive');
+  headerCategoryTitle.classList.add('inactive');
+  searchForm.classList.add('inactive');
+
+  trendingPreviewSection.classList.add('inactive');
+  categoriesPreviewSection.classList.add('inactive');
+  genericSection.classList.add('inactive');
+  movieDetailSection.classList.remove('inactive');
+
+  // ['#movie', '234567']
+  const [_, movieId] = location.hash.split('=');
+  getMovieById(movieId);
+}
+
+function searchPage() {
+  console.log('Search!!');
+
+  headerSection.classList.remove('header-container--long');
+  headerSection.style.background = '';
+  arrowBtn.classList.remove('inactive');
+  arrowBtn.classList.remove('header-arrow--white');
+  headerTitle.classList.add('inactive');
+  headerCategoryTitle.classList.add('inactive');
+  searchForm.classList.remove('inactive');
+
+  trendingPreviewSection.classList.add('inactive');
+  categoriesPreviewSection.classList.add('inactive');
+  genericSection.classList.remove('inactive');
+  movieDetailSection.classList.add('inactive');
+
+  // ['#search', 'platzi']
+  const [_, query] = location.hash.split('=');
+  getMoviesBySearch(query);
+}
+
+function trendsPage() {
+  console.log('TRENDS!!');
+
+  headerSection.classList.remove('header-container--long');
+  headerSection.style.background = '';
+  arrowBtn.classList.remove('inactive');
+  arrowBtn.classList.remove('header-arrow--white');
+  headerTitle.classList.add('inactive');
+  headerCategoryTitle.classList.remove('inactive');
+  searchForm.classList.add('inactive');
+
+  trendingPreviewSection.classList.add('inactive');
+  categoriesPreviewSection.classList.add('inactive');
+  genericSection.classList.remove('inactive');
+  movieDetailSection.classList.add('inactive');
+
+  headerCategoryTitle.innerHTML = 'Tendencias';
+
+  getTrendingMovies();
+}
